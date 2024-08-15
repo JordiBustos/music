b0e507bedb05d2df73483f87fa5864427ec26268
Jordi Bustos <jordibustos01@gmail.com>
2024-08-15 00:45:52 -0300

    added statistics module and trending view

---
+++ web/sites/default/files/php/twig/66bd797775d5a_node.html.twig_R3penu4GimmpeZ0YF7Bmvtwpu/j1RWK5nsF-54g7yIW7vI_Aa4_sS40vDD_-AY07bSnko.php
@@ -0,0 +1,168 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/stable9/templates/content/node.html.twig */
+class __TwigTemplate_538dda50d54c976916a5a689f47f9ab7 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension(SandboxExtension::class);
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 64
+        yield "<article";
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null), 64, $this->source), "html", null, true);
+        yield ">
+
+  ";
+        // line 66
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_prefix"] ?? null), 66, $this->source), "html", null, true);
+        yield "
+  ";
+        // line 67
+        if ((($context["label"] ?? null) &&  !($context["page"] ?? null))) {
+            // line 68
+            yield "    <h2";
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_attributes"] ?? null), 68, $this->source), "html", null, true);
+            yield ">
+      <a href=\"";
+            // line 69
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["url"] ?? null), 69, $this->source), "html", null, true);
+            yield "\" rel=\"bookmark\">";
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["label"] ?? null), 69, $this->source), "html", null, true);
+            yield "</a>
+    </h2>
+  ";
+        }
+        // line 72
+        yield "  ";
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_suffix"] ?? null), 72, $this->source), "html", null, true);
+        yield "
+
+  ";
+        // line 74
+        if (($context["display_submitted"] ?? null)) {
+            // line 75
+            yield "    <footer>
+      ";
+            // line 76
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["author_picture"] ?? null), 76, $this->source), "html", null, true);
+            yield "
+      <div";
+            // line 77
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["author_attributes"] ?? null), 77, $this->source), "html", null, true);
+            yield ">
+        ";
+            // line 78
+            yield t("Submitted by @author_name on @date", array("@author_name" => ($context["author_name"] ?? null), "@date" => ($context["date"] ?? null), ));
+            // line 79
+            yield "        ";
+            yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["metadata"] ?? null), 79, $this->source), "html", null, true);
+            yield "
+      </div>
+    </footer>
+  ";
+        }
+        // line 83
+        yield "
+  <div";
+        // line 84
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content_attributes"] ?? null), 84, $this->source), "html", null, true);
+        yield ">
+    ";
+        // line 85
+        yield $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null), 85, $this->source), "html", null, true);
+        yield "
+  </div>
+
+</article>
+";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["attributes", "title_prefix", "label", "page", "title_attributes", "url", "title_suffix", "display_submitted", "author_picture", "author_attributes", "author_name", "date", "metadata", "content_attributes", "content"]);        return; yield '';
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "core/themes/stable9/templates/content/node.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array (  101 => 85,  97 => 84,  94 => 83,  86 => 79,  84 => 78,  80 => 77,  76 => 76,  73 => 75,  71 => 74,  65 => 72,  57 => 69,  52 => 68,  50 => 67,  46 => 66,  40 => 64,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/stable9/templates/content/node.html.twig", "/var/www/html/web/core/themes/stable9/templates/content/node.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("if" => 67, "trans" => 78);
+        static $filters = array("escape" => 64);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if', 'trans'],
+                ['escape'],
+                [],
+                $this->source
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
