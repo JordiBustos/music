78fb935abc3a63839853269a5989e7f4c8e6dd7d
Jordi Bustos <jordibustos01@gmail.com>
2024-08-17 14:35:45 -0300

    fix: added missing ajax and dropdown submit on change

--- web/themes/music/js/music.js
+++ web/themes/music/js/music.js
@@ -2,10 +2,24 @@
  * @file
  * music behaviors.
  */
-(function (Drupal) {
+(function (Drupal, $) {
   "use strict";
 
   Drupal.behaviors.music = {
-    attach(context, settings) {},
+    attach(context, settings) {
+      const dropdowns = [
+        "[data-drupal-selector=edit-field-era-target-id]",
+        "[data-drupal-selector=edit-field-instrument-target-id]",
+        "[data-drupal-selector=edit-field-nationality-value]",
+      ];
+
+      const submit = $("[data-drupal-selector=edit-actions] input");
+
+      dropdowns.forEach((dropdown) => {
+        $(dropdown, context).on("change", function () {
+          submit.trigger("click");
+        });
+      });
+    },
   };
-})(Drupal);
+})(Drupal, jQuery);
